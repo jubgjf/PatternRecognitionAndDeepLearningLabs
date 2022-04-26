810725c9c9439e2a520fd2e33da46b6d5f7e5f8c
jubgjf <guanjiannan@outlook.com>
2022-04-26 16:42:37 +0800

    [DL lab1] add report

---
+++ report/DL-lab1/load_model.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    load_model = False  # 是否加载已有模型
+    model_path = "./model/model.pt"
+    ...
+
+if __name__ == "__main__":
+    load_model = True  # 是否加载已有模型
+    model_path = "./model/model.pt"
+    ...
